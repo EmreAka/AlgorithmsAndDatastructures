d1f623cf93075977a85e3bc9693de4c6b00c9099
Emre AKA <aka548548@gmail.com>
2023-11-25 15:13:23 +0300

    binary search

---
+++ binary-search.ts
@@ -0,0 +1,30 @@
+const sortedArray: number[] = [
+    6, 10, 20, 31, 51, 356, 3231, 5666
+];
+
+const index = binarySearch(5666, sortedArray);
+console.log(index);
+
+
+function binarySearch(searchNumber: number, numbers: number[]): number {
+    let left = 0
+    let right = numbers.length - 1
+
+    while (left <= right) {
+        let mid = Math.round(left + (right-left / 2))
+
+        if (searchNumber === numbers[mid]) {
+            return mid
+        }
+
+        else if (searchNumber > numbers[mid]) {
+            left = mid + 1
+        }
+
+        else {
+            right = mid - 1
+        }
+    }
+
+    return -1
+}
