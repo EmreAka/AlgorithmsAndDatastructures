221084a0ac77846807aa20149a7eb270fa60636a
Emre AKA <aka548548@gmail.com>
2025-04-03 00:12:31 +0300

    refactor

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,6 +1,6 @@
 ﻿Span<int> sortedList = stackalloc int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-Console.WriteLine(BinarySearch(sortedList, 10));
+Console.WriteLine(BinarySearch(sortedList, 9));
 return;
 
 static int BinarySearch(Span<int> source, int target)
@@ -9,8 +9,6 @@ static int BinarySearch(Span<int> source, int target)
 
     while (span.LeftIndex <= span.RightIndex)
     {
-        span.CalculateMiddleIndex();
-
         var middleValue = source[span.MiddleIndex];
 
         if (middleValue == target) return span.MiddleIndex;
@@ -25,17 +23,12 @@ internal ref struct Span
 {
     public int LeftIndex { get; set; }
     public int RightIndex { get; set; }
-    public int MiddleIndex { get; set; }
+    public int MiddleIndex => LeftIndex + (RightIndex - LeftIndex) / 2;
 
     public Span(int leftIndex, int rightIndex)
     {
         LeftIndex = leftIndex;
         RightIndex = rightIndex;
-    }
-
-    public void CalculateMiddleIndex()
-    {
-        MiddleIndex = LeftIndex + (RightIndex - LeftIndex) / 2;
     }
 
     public void MoveLeft() => RightIndex = MiddleIndex - 1;
